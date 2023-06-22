633cbb9d56766cf95c481dcd040bed058fcd4c70
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-06-22 17:15:16 +0700

    Revert "initial project"
    This reverts commit 691e2ab89b8d1c83326bc217b9f4f84b69c68a91.
    
    	deleted:    ecourse/authen/admin.py
    	deleted:    ecourse/authen/apps.py
    	deleted:    ecourse/authen/models.py
    	deleted:    ecourse/authen/tests.py
    	deleted:    ecourse/authen/views.py

--- ecourse/ecourse/settings/settings_dev.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-
-DEBUG=True
-INSTALLED_APPS.append('debug_toolbar')
-MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
