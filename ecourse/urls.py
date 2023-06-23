0c5b4af8d2205e3f60639de459a5233551b7047c
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-06-23 14:42:29 +0700

    fix debug tool bar url for dev enviroment

--- ecourse/urls.py
+++ ecourse/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
+    path("admin/", admin.site.urls),    
 ]
+
+if settings.DEBUG == True:
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
