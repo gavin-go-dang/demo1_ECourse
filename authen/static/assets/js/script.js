8a3b8a12cb123887344a36d3995ca51d892fca64
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-08-10 11:34:55 +0700

    fix UI

--- authen/static/assets/js/script.js
+++ authen/static/assets/js/script.js
@@ -1,11 +1,33 @@
-$('#password, #confirm_password').on('keyup', function () {
-  const password = $('#password').val();
-  const confirm_password = $('#confirm_password').val();
-  if (password === confirm_password && password !== '') {
-    $('#message').html('Matching').css('color', 'green');
-    $('#btnSubmit').prop('disabled', false);
-  } else {
-    $('#message').html('Not Matching').css('color', 'red');
-    $('#btnSubmit').prop('disabled', true);
-  }
+$(document).ready(function () {
+  console.log('start')
+  // Lấy các phần tử form
+  const form = $('#registrationForm');
+  const usernameInput = $('#username');
+  const fullNameInput = $('#full_name');
+  const emailInput = $('#email');
+  const passwordInput = $('#password');
+  const confirmPasswordInput = $('#confirm_password');
+  const submitButton = $('#btnSubmit');
+
+  // Thêm sự kiện nghe khi người dùng nhập dữ liệu
+  form.on('input', function () {
+    // Kiểm tra điều kiện và kiểm tra mật khẩu
+    console.log('check')
+    if (
+      usernameInput.val() !== '' &&
+      fullNameInput.val() !== '' &&
+      emailInput.val() !== '' &&
+      passwordInput.val() !== '' &&
+      confirmPasswordInput.val() !== '' &&
+      passwordInput.val() === confirmPasswordInput.val()
+    ) {
+      console.log('OK')
+      submitButton.prop('disabled', false); 
+      $('#input-submit').css('background-color', 'blue');
+    } else {
+      console.log('not ok')
+      submitButton.prop('disabled', true); 
+      $('#input-submit').css('background-color', '#d58cff');
+    }
+  });
 });
